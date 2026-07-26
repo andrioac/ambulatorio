12440de121d201e92c5410c3a7446a6e500b5c53
andrioac <andrio.ac@gmail.com>
2026-07-25 22:46:03 -0300

    feat(seguranca): registra middleware HTTP

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,10 +1,11 @@
 <?php
 
+use App\Http\Middleware\AdicionarCabecalhosSeguranca;
+use App\Http\Middleware\HandleInertiaRequests;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
-use App\Http\Middleware\HandleInertiaRequests;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -13,7 +14,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->web(append: [HandleInertiaRequests::class]);
+        $middleware->web(append: [
+            HandleInertiaRequests::class,
+            AdicionarCabecalhosSeguranca::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
