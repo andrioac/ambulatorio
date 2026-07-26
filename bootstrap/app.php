72ee07ae32fe218792282f88d98fcae359c5bc7e
andrioac <andrio.ac@gmail.com>
2026-07-26 19:22:16 -0300

    style: remove importacao global redundante

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -9,7 +9,6 @@ use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Symfony\Component\HttpFoundation\Response;
-use Throwable;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
