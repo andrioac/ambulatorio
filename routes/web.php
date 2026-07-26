5f28ff2ef8440c7eefbb1ce7381504f6c0823b06
andrioac <andrio.ac@gmail.com>
2026-07-26 13:13:13 -0300

    feat(profissionais): registra rotas administrativas

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AutenticacaoController;
+use App\Http\Controllers\ProfissionalController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -12,4 +13,12 @@ Route::middleware('guest')->group(function (): void {
 Route::middleware('auth')->group(function (): void {
     Route::get('/', fn () => Inertia::render('Painel'))->name('inicio');
     Route::post('/sair', [AutenticacaoController::class, 'destruir'])->name('logout');
+
+    Route::get('/profissionais', [ProfissionalController::class, 'index'])->name('profissionais.index');
+    Route::get('/profissionais/novo', [ProfissionalController::class, 'create'])->name('profissionais.create');
+    Route::post('/profissionais', [ProfissionalController::class, 'store'])->name('profissionais.store');
+    Route::get('/profissionais/{profissional}/editar', [ProfissionalController::class, 'edit'])->name('profissionais.edit');
+    Route::put('/profissionais/{profissional}', [ProfissionalController::class, 'update'])->name('profissionais.update');
+    Route::post('/profissionais/{profissional}/vinculos', [ProfissionalController::class, 'salvarVinculo'])->name('profissionais.vinculos.store');
+    Route::delete('/profissionais/{profissional}/vinculos/{vinculo}', [ProfissionalController::class, 'removerVinculo'])->name('profissionais.vinculos.destroy');
 });
