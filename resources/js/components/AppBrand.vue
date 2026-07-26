2a960fd9f09af95b455a5370d992e64513092e3b
andrioac <andrio.ac@gmail.com>
2026-07-26 00:35:18 -0300

    feat(interface): adiciona componente da marca

---
+++ resources/js/components/AppBrand.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+withDefaults(defineProps<{ compacta?: boolean; clara?: boolean }>(), { compacta: false, clara: false });
+</script>
+
+<template>
+    <div class="app-brand" :class="{ 'app-brand--compacta': compacta, 'app-brand--clara': clara }">
+        <img class="app-brand__marca" src="/brand/logo-mark.svg" alt="" />
+        <div v-if="!compacta" class="app-brand__texto">
+            <strong>Ambulatório Inteligente</strong>
+            <span>Cuidado e Atendimento Humanizado</span>
+        </div>
+    </div>
+</template>
