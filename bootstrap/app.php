2ca64959938187fcea43f0b0ef29de9fceb98999
andrioac <andrio.ac@gmail.com>
2026-07-26 15:42:13 -0300

    fix: corrige assinatura do tratamento de erros Laravel

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -9,6 +9,7 @@ use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Symfony\Component\HttpFoundation\Response;
+use Throwable;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -28,10 +29,11 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
-            fn (Request $request) => $request->is('api/*'),
+            fn (Request $request, Throwable $exception) => $request->is('api/*') || $request->expectsJson(),
         );
 
-        $exceptions->respond(function (Response $response, Throwable $exception, Request $request): Response {
+        $exceptions->respond(function (Response $response): Response {
+            $request = request();
             $status = $response->getStatusCode();
 
             if (! $request->expectsJson() && in_array($status, [403, 404, 419, 422], true)) {
